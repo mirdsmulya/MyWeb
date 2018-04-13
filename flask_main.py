97938abe3aa2254a103e42b91242572047981fba
mirdsm <by_ms19@yahoo.com>
2018-04-13 17:06:10 +0700

    second phase

--- flask_main.py
+++ flask_main.py
@@ -3,21 +3,30 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap =Bootstrap(app)
+app._static_folder = "/home/mirdsm/project/flasky"
 
 @app.route('/')
 def main():
 	return render_template('main_home.html')
 
+@app.route('/para')
+def parra():
+	return render_template('parralax.html')
+
 
 @app.route('/user-agent')
 def user():
-	user_agennt = request.header.get('User-agent')
-	return '<p> Your browser is %s </p>'% user_agennt
+	user_agent = request.header.get('User-agent')
+	return '<p> Your browser is %s </p>'% user_agent
 
 @app.route('/profile')
 def profile():
 	name = 'Mirdan Syahid'
 	return render_template('user.html')
+
+@app.route('/demo')
+def demo():
+	return render_template('parallax-demo.html')
 
 
 if __name__ == '__main__':
